1b1e8a54af0d53539606ef156de30cd9a7f2c84e
katsuhirokiyomoto <katsuhiro.k@hotmail.com>
2017-06-17 01:20:55 +0000

    taskist complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,25 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy] 
+  before_action :correct_user, only: [:edit, :destroy]
   
   def index
-    @tasks=Task.order(created_at: :desc).page(params[:page]).per(3)
+    @user=current_user
+    @tasks=@user.tasks.order(created_at: :desc).page(params[:page]).per(3)
+    
   end
  
   def show
   end
 
   def new
-    @task=Task.new
+    @user=current_user
+    @task=@user.tasks.new
   end
   
   def create
-    @task=Task.new(task_params)
+    @user=current_user
+    @task=@user.tasks.new(task_params)
     
     if @task.save
       flash[:success]='タスクが正常に作成されました'
@@ -39,15 +45,20 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success]='タスクが正常に消去されました'
     redirect_to tasks_url
   end
   
   def set_task
-    @task=Task.find(params[:id])
+    @task=current_user.tasks.find(params[:id])
   end
   
+  def correct_user
+    @task=current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+  end
 # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
